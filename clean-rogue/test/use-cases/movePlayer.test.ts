73cbc060216c9cb6648b202e2cc9ec252a595e4c
Brian Butz <butzopower@gmail.com>
2017-01-03 21:49:26 +0000

    Setup tslint

--- clean-rogue/test/use-cases/movePlayer.test.ts
+++ clean-rogue/test/use-cases/movePlayer.test.ts
@@ -1,11 +1,11 @@
-import { expect } from 'chai';
+import { expect } from "chai";
 import { Direction } from "../../lib/entities/direction";
 import { Player } from "../../lib/entities/player";
 import { Room } from "../../lib/entities/room";
 import { movePlayer, MovePlayerResult } from "../../lib/use-cases/movePlayer";
 
-describe('moving a player', () => {
-  it('should not allow a player to move in a 1x1 room', (done) => {
+describe("moving a player", () => {
+  it("should not allow a player to move in a 1x1 room", (done) => {
     let player: Player = { x: 0, y: 0 };
     let room: Room = { width: 1, height: 1 };
 
